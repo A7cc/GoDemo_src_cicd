6d2c9eec86295564e20a961ec15ce196c2705495
a7cc <47489441+A7cc@users.noreply.github.com>
2026-07-10 11:59:40 +0800

    code fix_v1

--- main.go
+++ main.go
@@ -16,17 +16,6 @@ func execHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "%s", result)
 }
-
-func search(w http.ResponseWriter, r *http.Request) {
-
-	id := r.URL.Query().Get("id")
-
-	query := "select * from users where id=" + id
-
-	db.Query(query)
-
-}
-
 func main() {
 
 	var password = "123456"
