85e895977c50f52cde542e4259bcccee0f2d0725
a7cc <47489441+A7cc@users.noreply.github.com>
2026-07-10 12:48:37 +0800

    code fix_v2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module demo-cicd
 
 go 1.22.0
+
+require github.com/mattn/go-sqlite3 v1.14.47 // indirect
--- main.go
+++ main.go
@@ -1,28 +1,60 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
-	"os/exec"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func execHandler(w http.ResponseWriter, r *http.Request) {
-	cmd := r.URL.Query().Get("cmd")
-	// 危险示例：用户输入直接进入系统命令
-	result, err := exec.Command("sh", "-c", cmd).Output()
+var db *sql.DB
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	// SQL注入漏洞
+	query := "SELECT username FROM users WHERE id=" + id
+	rows, err := db.Query(query)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	fmt.Fprintf(w, "%s", result)
+	defer rows.Close()
+	for rows.Next() {
+		var username string
+		rows.Scan(&username)
+		fmt.Fprintf(w, username)
+	}
 }
-func main() {
 
-	var password = "123456"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello ci/cd"))
-	})
-	http.HandleFunc("/ping", execHandler)
-	fmt.Println(password)
-	http.ListenAndServe(":8080", nil)
+func initDB() {
+	var err error
+	db, err = sql.Open(
+		"sqlite3",
+		"./test.db",
+	)
+	if err != nil {
+		panic(err)
+	}
+	db.Exec(`
+	CREATE TABLE IF NOT EXISTS users(
+		id INTEGER,
+		username TEXT
+	)
+	`)
+	db.Exec(`
+	INSERT INTO users VALUES(1,'admin')
+	`)
+}
+
+func main() {
+	initDB()
+	http.HandleFunc(
+		"/search",
+		searchHandler,
+	)
+	http.ListenAndServe(
+		":8080",
+		nil,
+	)
 }
